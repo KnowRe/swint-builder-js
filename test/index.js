5588f57bdbbf1c678d7eba785ce726cfcfff6cef
Kivol <kivoloid@gmail.com>
2015-10-13 12:03:26 +0900

    Removed error logging on test

--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ var os = require('os'),
 	swintHelper = require('swint-helper'),
 	buildJS = require('../lib');
 
+global.swintVar.printLevel = 5;
+
 describe('builder-js', function() {
 	it('Error when no callback', function() {
 		assert.throws(function() {
