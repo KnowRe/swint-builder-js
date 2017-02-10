04eed59133ef9f0832e35d02aae09ed757dc03eb
sunghwancho <sunghwancho2011@gmail.com>
2017-02-10 15:40:47 +0900

    Update: Added 'useSourceMap' option when minified

--- test/index.js
+++ test/index.js
@@ -76,10 +76,42 @@ describe('builder-js', function() {
 		});
 	});
 
+	it('Minified (useSourceMap: false) case', function(done) {
+		buildJS({
+			name: 'Test',
+			inDir: path.join(__dirname, '../test_case'),
+			outDir: path.join(os.tmpdir(), 'swint-builder-js-out2'),
+			minify: true,
+			lint: {
+				check: false
+			},
+			useSourceMap: false,
+			variables: {
+				tmplVar: 'A'
+			}
+		}, function(err, res) {
+			assert.deepEqual(
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-js-out2/Test.js'), 'utf-8'),
+				fs.readFileSync(path.join(__dirname, '../test_result/common.js'), 'utf-8')
+			);
+
+			assert.deepEqual(
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-js-out2/Test.min.js'), 'utf-8'),
+				fs.readFileSync(path.join(__dirname, '../test_result/common2.min.js'), 'utf-8')
+			);
+
+			done();
+		});
+	});
+
 	after(function() {
 		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-js-out/Test.js'));
 		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-js-out/Test.min.js'));
 		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-js-out/Test.min.js.map'));
 		fs.rmdirSync(path.join(os.tmpdir(), 'swint-builder-js-out'));
+
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-js-out2/Test.js'));
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-js-out2/Test.min.js'));
+		fs.rmdirSync(path.join(os.tmpdir(), 'swint-builder-js-out2'));
 	});
 });
